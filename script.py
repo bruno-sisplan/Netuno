a945c1a3369bb4216622a30e4eb8c701efcd55cb
bruno-sisplan <bruno.sena@sisplansistemas.com.br>
2024-08-27 17:29:16 -0300

    update

--- script.py
+++ script.py
@@ -8,6 +8,9 @@ import threading
 import queue
 import webbrowser as browser
 import requests as r
+import locale
+import re
+from pywinauto import Application
 #import speech_recognition as sr 
 #import urllib.request, json, requests
 #import translateimport urllib.request, json, requests
@@ -23,6 +26,7 @@ CORS(app)
 
 response_queue = queue.Queue()
 response_event = threading.Event() 
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 def clima(cidade):
     token = "eb2ebd443b002b99bf5fd10c42fd625e"
@@ -52,91 +56,140 @@ def cotacao(moeda):
 	nome = cotacao[moeda]['name']
 	data = cotacao[moeda]['create_date']
 	valor = cotacao[moeda]['bid']
-	return f'Cotação do {nome} em {data} é {valor}'
+	return f'Cotação do {nome} em {data} é {locale.currency(float(valor), grouping=True)}'
 
 def executa_comandos(mensagem):
-    mensagem = mensagem.lower()
+    mensagem = re.findall(r'\{.*?\}|\S+', mensagem.lower().replace('?', '').replace('!', ''))
     response_mensagem = ''
-    cumprimentos = ['boa noite', 'bom dia', 'boa tarde', 'oi', 'olá', 'ola', 'salve']
-    
-    if any(cumprimento in mensagem for cumprimento in cumprimentos):
-        x = ''
+    cumprimentos = ['noite', 'dia', 'tarde', 'oi', 'olá', 'ola', 'salve', 'eai', 'netuno']
+    palavras_cotacoes = ['cotação', 'cotacao', 'cota']
+    palavras_desligar_computador = ['desligar', 'tchau', 'embora', 'amanhã']
+    palavras_pesquisa_navegador = ['pesquisar', 'pesquisa', 'joga']
+
+    if any(cumprimento in cumprimentos for cumprimento in mensagem):
+        indice_c = -1
+        response_mensagem = ''
         for c in cumprimentos:
             if c in mensagem:
-                x = c
-        agora = datetime.now().time()
-        inicio_dia = datetime.strptime('05:00:00', '%H:%M:%S').time()
-        inicio_tarde = datetime.strptime('12:00:00', '%H:%M:%S').time()
-        inicio_noite = datetime.strptime('18:00:00', '%H:%M:%S').time()
-        saudacao = ''
-        response_mensagem = ''
-        if inicio_dia <= agora < inicio_tarde:
-            saudacao = "Bom dia"
-        elif inicio_tarde <= agora < inicio_noite:
-            saudacao = "Boa tarde"
-        else:
-            saudacao = "Boa noite"
-        
-        if x == 'salve':
-            response_mensagem = 'E ai, meu faixa. O que tu manda hoje?'
-        elif x == 'oi':
-            response_mensagem = 'Oi. O que você precisa, Bruno?'
-        elif x == 'olá' or x == 'ola':
-            response_mensagem = 'Olá, Bruno. Do que precisa?'
-        else:
-            response_mensagem = f'{saudacao}, Bruno! Como posso te ajudar hoje?'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        
-    #fechar assistente
-    if 'fechar assistente' in mensagem:
-        response_mensagem = 'Até logo!'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        t.sleep(1)
-        s._exit(0)
+                if c == 'salve':
+                    response_mensagem = 'E ai, meu faixa. O que tu manda hoje?'
+                elif c == 'oi':
+                    response_mensagem = 'Oi. O que você precisa, Bruno?'
+                elif c == 'olá' or c == 'ola':
+                    response_mensagem = 'Olá, Bruno. Do que precisa?'
+                else:
+                    indice_c = mensagem.index(c)
+                    agora = datetime.now().time()
+                    inicio_dia = datetime.strptime('05:00:00', '%H:%M:%S').time()
+                    inicio_tarde = datetime.strptime('12:00:00', '%H:%M:%S').time()
+                    inicio_noite = datetime.strptime('18:00:00', '%H:%M:%S').time()
+                    saudacao = ''
+                    if inicio_dia <= agora < inicio_tarde:
+                        saudacao = "Bom dia"
+                    elif inicio_tarde <= agora < inicio_noite:
+                        saudacao = "Boa tarde"
+                    else:
+                        saudacao = "Boa noite"
+                    if c == 'dia':
+                        if (mensagem[indice_c - 1]) == 'bom':
+                            response_mensagem = f'{saudacao}, Bruno! Como posso te ajudar hoje?'
+                    elif c == 'tarde' or c == 'noite':
+                        if (mensagem[indice_c - 1]) == 'boa':
+                            response_mensagem = f'{saudacao}, Bruno! Como posso te ajudar hoje?'
+                response_queue.put(response_mensagem)
+                response_event.set()
+                    
+    # #fechar assistente
+    # if 'fechar assistente' in mensagem:
+    #     response_mensagem = 'Até logo!'
+    #     response_queue.put(response_mensagem)
+    #     response_event.set()
+    #     t.sleep(1)
+    #     s._exit(0)
 
     #desligar computador
-    elif 'desligar computador' in mensagem and 'uma hora' in mensagem:
-        response_mensagem = 'Computador agendado pra desligar daqui a uma hora.'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        o.system("shutdown /s /f /t 3600")
-    elif 'desligar computador' in mensagem and 'meia hora' in mensagem:
-        response_mensagem = 'Computador agendado pra desligar daqui a meia hora.'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        o.system("shutdown /s /f /t 1800")
-    elif 'desligar computador' in mensagem and 'agora' in mensagem:
-        response_mensagem = 'Computador irá desligar em 5 segundos.'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        t.sleep(5)
-        o.system("shutdown /s /f /t 0")
-    elif 'cancelar desligamento' in mensagem:
-        o.system("shutdown -a")
-        
-        
+    if any(palavra_desliga_pc in palavras_desligar_computador for palavra_desliga_pc in mensagem):
+        indice_c = -1
+        response_mensagem = ''
+        for c in palavras_desligar_computador:
+            if c in mensagem:
+                if c == 'desligar':
+                    indice_c = mensagem.index(c)
+                    if mensagem[indice_c + 1] == 'computador':
+                        if 'uma' in mensagem and 'hora' in mensagem:
+                            response_mensagem = 'Computador agendado pra desligar daqui a uma hora.'
+                            response_queue.put(response_mensagem)
+                            response_event.set()
+                            o.system("shutdown /s /f /t 3600")
+                        elif 'meia' in mensagem and 'hora' in mensagem:
+                            response_mensagem = 'Computador agendado pra desligar daqui a meia hora.'
+                            response_queue.put(response_mensagem)
+                            response_event.set()
+                            o.system("shutdown /s /f /t 1800")  
+                        elif '15' in mensagem and 'minutos' in mensagem:
+                            response_mensagem = 'Computador agendado pra desligar daqui a 15 minutos.'
+                            response_queue.put(response_mensagem)
+                            response_event.set()
+                            o.system("shutdown /s /f /t 900") 
+                        elif 'agora' in mensagem:
+                            response_mensagem = 'Computador irá desligar em 5 segundos.'
+                            response_queue.put(response_mensagem)
+                            response_event.set()
+                            t.sleep(5)
+                            o.system("shutdown /s /f /t 0")
+                        elif 'cancelar' in mensagem:
+                            response_mensagem = 'Retirado agendamento para desligar computador'
+                            response_queue.put(response_mensagem)
+                            response_event.set()
+                            o.system("shutdown /a")
+
+    if any(palavra_pesquisa in palavras_pesquisa_navegador for palavra_pesquisa in mensagem):
+        indice_c = -1
+        response_mensagem = ''
+        for c in palavras_pesquisa_navegador:
+            if c in mensagem:
+                if 'google' in mensagem: 
+                    indice_chave_pesquisa = next((i for i, s in enumerate(mensagem) if re.match(r'\{.*\}', s)), None) 
+                    response_mensagem = 'Pesquisando no google...'
+                    response_queue.put(response_mensagem)
+                    response_event.set()
+                    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+                    browser.get(chrome_path).open(f"https://google.com/search?q={mensagem[indice_chave_pesquisa].replace('{', '').replace('}', '')}")
+                elif 'youtube' in mensagem:
+                    indice_chave_pesquisa = next((i for i, s in enumerate(mensagem) if re.match(r'\{.*\}', s)), None) 
+                    response_mensagem = 'Pesquisando no youtube...'
+                    response_queue.put(response_mensagem)
+                    response_event.set()
+                    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+                    browser.get(chrome_path).open(f"https://youtube.com/results?search_query={mensagem[indice_chave_pesquisa].replace('{', '').replace('}', '')}")
+    
+    if 'gerar' and 'versao' in mensagem:
+        versao_path = r'C:/Users/bruno.sena/Documents/gerar_versao/gerar_versao.exe'
+        app = Application().start(versao_path)
+        t.sleep(3)
+        janela = app.window(title="gerar_versao")
+        janela.ElevatedButton.click()
+
     #pesquisar google    
-    elif 'pesquisar' in mensagem and 'google' in mensagem:  
-        response_mensagem = 'Pesquisando no google...'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        mensagem = mensagem.replace('pesquisar', '')
-        mensagem = mensagem.replace('google', '')
-        chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
-        browser.get(chrome_path).open(f"https://google.com/search?q={mensagem}")
+    # elif 'pesquisar' in mensagem and 'google' in mensagem:  
+    #     response_mensagem = 'Pesquisando no google...'
+    #     response_queue.put(response_mensagem)
+    #     response_event.set()
+    #     mensagem = mensagem.replace('pesquisar', '')
+    #     mensagem = mensagem.replace('google', '')
+    #     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    #     browser.get(chrome_path).open(f"https://google.com/search?q={mensagem}")
         
         
-    #pesquisar ytb 
-    elif 'pesquisar' in mensagem and 'youtube' in mensagem:
-        response_mensagem = 'Pesquisando no youtube...'
-        response_queue.put(response_mensagem)
-        response_event.set()
-        mensagem = mensagem.replace('pesquisar', '')
-        mensagem = mensagem.replace('youtube', '')
-        chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
-        browser.get(chrome_path).open(f'https://youtube.com/results?search_query={mensagem}')
+    # #pesquisar ytb 
+    # elif 'pesquisar' in mensagem and 'youtube' in mensagem:
+    #     response_mensagem = 'Pesquisando no youtube...'
+    #     response_queue.put(response_mensagem)
+    #     response_event.set()
+    #     mensagem = mensagem.replace('pesquisar', '')
+    #     mensagem = mensagem.replace('youtube', '')
+    #     chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+    #     browser.get(chrome_path).open(f'https://youtube.com/results?search_query={mensagem}')
         
     #clima e temperatura    
     elif 'clima' in mensagem:
@@ -145,6 +198,7 @@ def executa_comandos(mensagem):
         response_mensagem = clima(mensagem[2:])
         response_queue.put(response_mensagem)
         response_event.set()
+
     elif 'temperatura' in mensagem:
         mensagem = mensagem.replace('temperatura', '')
         mensagem = mensagem.replace('em', '')
@@ -153,19 +207,36 @@ def executa_comandos(mensagem):
         response_event.set()
         
     #cotação de moedas   
-    elif 'dólar' in mensagem:
-        response_mensagem = cotacao('USD')
-        response_queue.put(response_mensagem)
-        response_event.set()
-    elif 'euro' in mensagem:
-        response_mensagem = cotacao('EUR')
-        response_queue.put(response_mensagem)
-        response_event.set()
-  
-    elif 'bitcoin' in mensagem:
-        response_mensagem = cotacao('BTC')
-        response_queue.put(response_mensagem)
-        response_event.set()
+    # elif 'dólar' in mensagem:
+    #     response_mensagem = cotacao('USD')
+    #     response_queue.put(response_mensagem)
+    #     response_event.set()
+
+    if any(palavra_cotacao in palavras_cotacoes for palavra_cotacao in mensagem):
+        if 'dolar' in mensagem:
+            response_mensagem = cotacao('USD')
+            response_queue.put(response_mensagem)
+            response_event.set()
+        elif 'dólar' in mensagem:
+            response_mensagem = cotacao('USD')
+            response_queue.put(response_mensagem)
+            response_event.set()
+        elif 'euro' in mensagem:
+            response_mensagem = cotacao('EUR')
+            response_queue.put(response_mensagem)
+            response_event.set()
+        elif 'eur' in mensagem:
+            response_mensagem = cotacao('EUR')
+            response_queue.put(response_mensagem)
+            response_event.set()
+        elif 'bitcoin' in mensagem:
+            response_mensagem = cotacao('BTC')
+            response_queue.put(response_mensagem)
+            response_event.set()
+        elif 'btc' in mensagem:
+            response_mensagem = cotacao('BTC')
+            response_queue.put(response_mensagem)
+            response_event.set()
         
     #hora atual
     elif 'horas' in mensagem:
@@ -174,14 +245,18 @@ def executa_comandos(mensagem):
         response_queue.put(frase)
         response_event.set()       
     
+    
 @app.route('/usermessage', methods=['POST'])
 def server_run():
     data = request.json
     solicitacao = data.get('question')
     response_event.clear()
+    response_queue.queue.clear()
     executa_comandos_em_threads(solicitacao)    
     response_event.wait()
     mensagem_resposta = response_queue.get()
+    if mensagem_resposta == '':
+        mensagem_resposta = 'Não entendi. Pode repetir?'
     return jsonify({"message": mensagem_resposta}), 200
     
 if __name__ == '__main__':
